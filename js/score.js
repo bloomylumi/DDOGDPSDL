09e7995ff89188a385b775b8dab05f609154cd4e
Max-GD <suprememacaroni100@gmail.com>
2026-08-30 10:56:25 -0400

    Testing

--- js/score.js
+++ js/score.js
@@ -19,7 +19,7 @@ const tailFractionAtEnd     = 0.01;
 export function score(rank, percent, minPercent) {
     // If rank is outside the scoring range, return 0
     if (rank >= maxRank) {
-        return 10;
+        return 0;
     }
 
     // From rank 76+, only 100% counts
